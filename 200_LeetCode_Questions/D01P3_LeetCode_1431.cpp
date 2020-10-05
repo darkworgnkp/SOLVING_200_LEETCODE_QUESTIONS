8f74574ed1cb307a6830e126e60babbc0c5a14c8
KARTIK PUROHIT <58941118+darkworgnkp@users.noreply.github.com>
2020-10-05 22:06:06 +0530

    Update D01P3_LeetCode_1431.cpp
    Leetcode Problem 1431 .

--- 200_LeetCode_Questions/D01P3_LeetCode_1431.cpp
+++ 200_LeetCode_Questions/D01P3_LeetCode_1431.cpp
@@ -1,1 +1,32 @@
-# C++ code coming soon
+// Problem:
+
+// Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+// For each kid check if there is a way to distribute extraCandies among, 
+// the kids such that he or she can have the greatest number of candies among them,
+// Notice that multiple kids can have the greatest number of candies.
+
+// Solution:
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int mx = INT_MIN;
+        int n = candies.size();
+        for(int i=0;i<n;i++){
+            if(mx<candies[i]){
+                mx = candies[i];
+            }
+        }
+        vector<bool> arr;
+        for(int i=0;i<n;i++){
+            if((candies[i]+extraCandies)>=mx){
+                arr.push_back(true);
+            }
+            else{
+                arr.push_back(false);
+            }
+        }
+        return arr;
+    }
+};
